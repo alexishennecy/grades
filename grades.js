f074e7dfdfb3075c6cf393207027791aafcb8926
Alexis Hennecy <alexishennecy@users-MacBook-Pro.local>
2018-01-28 16:30:25 -0600

    setting up grades.js

---
+++ grades.js
@@ -0,0 +1,54 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+console.log("the highest grade is: ", Math.max.apply(null, scores));
+console.log("the lowest grade is: ", Math.min.apply(null, scores));
+
+// what is the lowest score?
+// what is the highest score?
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+console.log(scores.sort());
+
+
+
+A score of 50-60 is an F
+A score of 61-70 is a D
+A score of 71-80 is a C
+A score of 81-90 is a B
+A score of 91-100 is an A
+
+
+function returnScores() {
+    
+    }
+    
+    
+    
+const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+
+const grades = {}
+       
+
+
+
+if (scores > 90) {
+    console.log()
+
+}
+
+
+
+   
+    
+
+    
+        case "> 80 && < 91":
+            console.log("This is a B");
+            break;
+        case "> 70 && < 81":
+            console.log("This is a C");
+            break;
+        case "> 60 && < 71":
+            console.log("This is a D");
+            break;
+        default:
+            console.log("This is an F");
+}
