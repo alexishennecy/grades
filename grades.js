fc4cb63e12ea582721cb9f6b1850a32be2204629
Alexis Hennecy <alexishennecy@users-MacBook-Pro.local>
2018-01-31 15:54:16 -0600

    working on grades problem

--- grades.js
+++ grades.js
@@ -1,54 +1,71 @@
+//made an object to hold the ranges of values for grades
+
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
-console.log("the highest grade is: ", Math.max.apply(null, scores));
-console.log("the lowest grade is: ", Math.min.apply(null, scores));
-
-// what is the lowest score?
-// what is the highest score?
-var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
-console.log(scores.sort());
-
-
-
-A score of 50-60 is an F
-A score of 61-70 is a D
-A score of 71-80 is a C
-A score of 81-90 is a B
-A score of 91-100 is an A
+var grades = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0,
+    F: 0
+};
 
 
-function returnScores() {
-    
+for(i = 0; i < scores.length; i++){
+    if(scores[i] >= 90){
+        grades.A++;
+    }else if(scores[i] >= 80 && < 91){
+        grades.B++;
+    }else if(scores[i] >= 70 && < 81){
+        grades.C++;
+    }else if(scores[i] >= 60 && < 71){
+        grades.D++;
+    }else {
+        grades.F++;
     }
-    
-    
-    
-const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
 
-const grades = {}
+}
+
+
+console.log(grades.A);
+console.log(grades.B);
+
+
+// console.log("the highest grade is: ", Math.max.apply(null, scores));
+// console.log("the lowest grade is: ", Math.min.apply(null, scores));
+
+// // what is the lowest score?
+// // what is the highest score?
+// var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+// console.log(scores.sort());
+
+
+// A score of 50-60 is an F
+// A score of 61-70 is a D
+// A score of 71-80 is a C
+// A score of 81-90 is a B
+// A score of 91-100 is an A
+
+
+// function returnScores() {
+    
+//     }
+    
+    
+    
+// const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+
+// const grades = {};
        
 
 
 
-if (scores > 90) {
-    console.log()
+// if(scores > 90) {
+//     console.log()
 
-}
+// }
 
 
 
-   
-    
 
     
-        case "> 80 && < 91":
-            console.log("This is a B");
-            break;
-        case "> 70 && < 81":
-            console.log("This is a C");
-            break;
-        case "> 60 && < 71":
-            console.log("This is a D");
-            break;
-        default:
-            console.log("This is an F");
-}
+
